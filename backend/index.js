49871d9678622f95e2998fedd0c0c317f808fd39
Mic249 <jmika734@gmail.com>
2024-06-21 08:36:06 +0200

    routes

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,23 @@
-const app = require('./app')
+const app = require("./app");
 
-const port = process.env.PORT || 2000
+const port = process.env.PORT || 2000;
 
 // Handling uncaught Exception
-process.on('uncaughtException', err => {
-    console.log(`Error: ${err.message}`)
-    console.log(`shutting down the server for handling uncaught exception`)
-})
+process.on("uncaughtException", (err) => {
+	console.log(`Error: ${err.message}`);
+	console.log(`shutting down the server for handling uncaught exception`);
+});
 
 serveur = app.listen(port, () => {
-    console.log(
-        `Serveur runnging on : http://${process.env.HOST}:${process.env.PORT}`
-    )
-})
+	console.log(
+		`Serveur runnging on : http://${process.env.HOST}:${process.env.PORT}`
+	);
+});
 
 // unhandled promise rejection
-process.on('unhandledRejection', err => {
-    console.log(`Shutting down the server for : ${err}`)
-
-    server.close(() => {
-        process.exit(1)
-    })
-})
+process.on("unhandledRejection", (err) => {
+	console.log(`Shutting down the server for : ${err}`);
+	server.close(() => {
+		process.exit(1);
+	});
+});
