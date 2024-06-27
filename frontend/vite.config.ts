cae7b7c757656cb6d72e05f0816d61e17f2949b6
Mic249 <jmika734@gmail.com>
2024-06-27 23:48:42 +0200

    configuration du fichier pour que le projet react vite soit expose sur le LAN

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		host: true, // Écouter sur toutes les interfaces réseau
+		port: 5173, // Spécifiez le port que vous souhaitez utiliser (par exemple, 3000)
+	},
 });
